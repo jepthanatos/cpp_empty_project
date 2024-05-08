c3076c564362471a81c87c891ea64fed2e9059d7
Thanatos <jepthanatos@gmail.com>
2024-05-08 10:02:24 +0200

    [FEAT] New cpp empty project: needed files for an empty cpp project

---
+++ src/Main.cpp
@@ -0,0 +1,11 @@
+// System includes
+
+// Local includes
+
+// Namespaces
+// using namespace < namespace>;
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
---
+++ tests/TestExample.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+using namespace testing;
+
+TEST(TestExample, exampleFunction)
+{
+    auto level{0};
+    EXPECT_EQ(level, 0);
+}
